b127b09f64b4eba509056fc8e3d7d3e6f87ba5b6
Devesh Tulshyan <deveshtulshyan@gmail.com>
2023-04-12 16:11:27 +0530

    cookie unit test #2

--- server/app.js
+++ server/app.js
@@ -15,9 +15,9 @@ dotenv.config({
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(
     cors({
         origin: process.env.FRONTEND_URL,
